534663cc9b34204d2733c9d4c5b1c78db8a49133
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-20 12:06:12 -0300

    added: added connection to mongoose(Mongodb), defined schemas to connection, for possible inserctionCRUD.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://teste:teste123@cluster0.w9qfipe.mongodb.net/test'),
+    UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
